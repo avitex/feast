80a819f6a739561a343591f0c837e7f5ccadb445
avitex <theavitex@gmail.com>
2019-05-11 13:33:32 +1000

    Improve ascii recognition, add ascii digit parser

--- src/ascii.rs
+++ src/ascii.rs
@@ -3,47 +3,85 @@ use crate::pass::{Pass, PassInput, PassResult};
 
 use crate::core::*;
 
-pub fn ascii_alpha_lower<'p, P>(pass: P) -> PassResult<'p, P, u8>
-where
-    P: Pass<'p>,
-    PassInput<'p, P>: Input<'p, Token = u8>,
-{
-    hint(in_range(b'a', b'z'), "valid ascii alpha lower")(pass)
-}
-
-pub fn ascii_alpha_upper<'p, P>(pass: P) -> PassResult<'p, P, u8>
-where
-    P: Pass<'p>,
-    PassInput<'p, P>: Input<'p, Token = u8>,
-{
-    hint(in_range(b'A', b'Z'), "valid ascii alpha upper")(pass)
-}
-
-pub fn ascii_alpha<'p, P>(pass: P) -> PassResult<'p, P, u8>
+/// Consumes a byte if it is an ASCII lowercase character`[a-z]`.
+pub fn ascii_lowercase<'p, P>(pass: P) -> PassResult<'p, P, u8>
 where
     P: Pass<'p>,
     PassInput<'p, P>: Input<'p, Token = u8>,
 {
     hint(
-        or(ascii_alpha_lower, ascii_alpha_upper),
-        "valid ascii alpha",
+        take_token_if(u8::is_ascii_lowercase),
+        "valid ascii lowercase",
     )(pass)
 }
 
-pub fn ascii_alphanum<'p, P>(pass: P) -> PassResult<'p, P, u8>
+/// Consumes a byte if it is an ASCII uppercase character `[A-Z]`.
+pub fn ascii_uppercase<'p, P>(pass: P) -> PassResult<'p, P, u8>
 where
     P: Pass<'p>,
     PassInput<'p, P>: Input<'p, Token = u8>,
 {
-    hint(or(ascii_alpha, ascii_digit), "valid ascii alphanum")(pass)
+    hint(
+        take_token_if(u8::is_ascii_uppercase),
+        "valid ascii uppercase",
+    )(pass)
 }
 
+/// Consumes a byte if it is an ASCII alphabetic character `[a-zA-Z]`.
+pub fn ascii_alphabetic<'p, P>(pass: P) -> PassResult<'p, P, u8>
+where
+    P: Pass<'p>,
+    PassInput<'p, P>: Input<'p, Token = u8>,
+{
+    hint(
+        take_token_if(u8::is_ascii_alphabetic),
+        "valid ascii alphabetic",
+    )(pass)
+}
+
+/// Consumes a byte if it is an ASCII decimal digit `[0-9]`.
 pub fn ascii_digit<'p, P>(pass: P) -> PassResult<'p, P, u8>
 where
     P: Pass<'p>,
     PassInput<'p, P>: Input<'p, Token = u8>,
 {
-    hint(in_range(b'0', b'9'), "valid ascii digit")(pass)
+    hint(
+        take_token_if(u8::is_ascii_digit),
+        "valid ascii decimal digit",
+    )(pass)
+}
+
+/// Consumes a byte if it is an ASCII hexadecimal digit `[0-9A-Fa-f]`.
+pub fn ascii_hexdigit<'p, P>(pass: P) -> PassResult<'p, P, u8>
+where
+    P: Pass<'p>,
+    PassInput<'p, P>: Input<'p, Token = u8>,
+{
+    hint(
+        take_token_if(u8::is_ascii_hexdigit),
+        "valid ascii hex digit",
+    )(pass)
+}
+
+/// Consumes a byte if it is an ASCII alphanumeric character `[a-zA-Z0-9]`.
+pub fn ascii_alphanumeric<'p, P>(pass: P) -> PassResult<'p, P, u8>
+where
+    P: Pass<'p>,
+    PassInput<'p, P>: Input<'p, Token = u8>,
+{
+    hint(
+        take_token_if(u8::is_ascii_alphanumeric),
+        "valid ascii alphanumeric",
+    )(pass)
+}
+
+/// Parses a ASCII decimal digit.
+pub fn parse_ascii_digit<'p, P>(pass: P) -> PassResult<'p, P, u8>
+where
+    P: Pass<'p>,
+    PassInput<'p, P>: Input<'p, Token = u8>,
+{
+    map(ascii_digit, |t| t - b'0')(pass)
 }
 
 #[cfg(test)]
@@ -70,8 +108,15 @@ mod tests {
         let pass = test_pass(b"1");
 
         assert_matches!(
-            ascii_digit(pass),
+            ascii_digit(pass.clone()),
             Ok((b'1', pass_out)) => {
+                assert_eq!(pass_out, empty_pass())
+            }
+        );
+
+        assert_matches!(
+            ascii_digit_value(pass),
+            Ok((1, pass_out)) => {
                 assert_eq!(pass_out, empty_pass())
             }
         );
