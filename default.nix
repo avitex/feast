84ae9a968a7079198809073427006ef20a84faae
avitex <theavitex@gmail.com>
2019-09-04 20:43:51 +1000

    Use rust stable

--- default.nix
+++ default.nix
@@ -1,6 +1,19 @@
 with import <nixpkgs> {};
 
+let src = fetchFromGitHub {
+      owner = "mozilla";
+      repo = "nixpkgs-mozilla";
+      # commit from: 2019-05-15
+      rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+      sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
+   };
+in
+with import "${src.out}/rust-overlay.nix" pkgs pkgs;
+
 stdenv.mkDerivation {
     name = "feast-dev";
-    buildInputs = [ gcc cargo rustc ];
+
+    buildInputs = [
+        latest.rustChannels.stable.rust
+    ];
 }
